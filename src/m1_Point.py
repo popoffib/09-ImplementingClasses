f6a004898e2de7d80c493a18760309b82b56c066
popoffib <popoffib@rose-hulman.edu>
2019-02-11 20:58:28 -0500

    made more tests

--- src/m1_Point.py
+++ src/m1_Point.py
@@ -241,13 +241,32 @@ def run_test_repr():
         print('Actual for p2:  ', p2)
     """
     # -------------------------------------------------------------------------
-    # TODO: 4.  Follow the same instructions as in _TODO_ 3 above,
+    # DONE: 4.  Follow the same instructions as in _TODO_ 3 above,
     #           but for the  __repr__  method specified above.
     # -------------------------------------------------------------------------
     print()
     print('-----------------------------------------------------------')
     print('Testing the   __repr__   method of the Point class.')
     print('-----------------------------------------------------------')
+
+    p1 = Point(30, 18)
+    print()
+    print('Expected for p1: Point(30, 18)')
+    print('Actual for p1: ', p1)
+
+    p2 = Point(100, -40)
+    print()
+    print('Expected for p2: Point(100, -40)')
+    print('Actual for p2: ', p2)
+    print('Expected for p1: Point(30, 18)')
+    print('Actual for p1: ', p1)
+
+    p1.y = 999
+    print()
+    print('Expected for p1: Point(30, 999)')
+    print('Actual for p1: ', p1)
+    print('Expected for p2: Point(100, -40)')
+    print('Actual for p2: ', p2)
 
 
 def run_test_clone():
