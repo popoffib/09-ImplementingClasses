04c9ab3b5be90b5caa3f466925c1c40a26299ef6
popoffib <popoffib@rose-hulman.edu>
2019-02-11 20:53:00 -0500

    made tests for points

--- src/m1_Point.py
+++ src/m1_Point.py
@@ -180,7 +180,26 @@ def run_test_init():
     print('Testing the   __init__   method of the Point class.')
     print('-----------------------------------------------------------')
 
+    p1 = Point(40, 20)
+    print()
+    print('Expected for p1: 40 20')
+    print('Actual for p1: ', p1.x, p1.y)
 
+    p2 = Point(100, -50)
+    print()
+    print('Expected for p2: 100 -50')
+    print('Actual for p2: ', p2.x, p2.y)
+    print('Expected for p1: 40 20')
+    print('Actual for p1: ', p1.x, p1.y)
+
+    p1.y = 999
+    print()
+    print('Expected for p1: 40 999')
+    print('Actual for p1: ', p1.x, p1.y)
+    print('Expected for p2: 100 -50')
+    print('Actual for p2: ', p2.x, p2.y)
+
+    
 def run_test_repr():
     """
     Tests the   __repr__   method of the Point class.
